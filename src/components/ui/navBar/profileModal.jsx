3ddfc80cd5b140cfd4f4406b74aa6252633a563c
KhangKhangKhangKhang <duongtrongkhang121005@gmail.com>
2025-12-16 22:13:21 +0700

    level

--- src/components/ui/navBar/profileModal.jsx
+++ src/components/ui/navBar/profileModal.jsx
@@ -255,16 +255,15 @@ const ProfileModal = ({ isOpen, onClose }) => {
             <label className="block text-sm font-medium text-gray-700 mb-1">
               Trình độ
             </label>
-            <select
+            <button
               name="level"
               value={formData.level}
               onChange={handleChange}
-              className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+              className="w-full px-3 py-2 border bg-slate-700 text-white border-gray-300 rounded-md
+              focus:outline-none focus:ring-2 focus:ring-blue-500"
             >
-              <option value="Low">Thấp</option>
-              <option value="Mid">Trung bình</option>
-              <option value="High">Cao</option>
-            </select>
+              {formData.level}
+            </button>
           </div>
 
           {/* Buttons */}
