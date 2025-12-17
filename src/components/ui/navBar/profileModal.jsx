9be6b6d1109bbed01c1ae1aa4aa2f19c782b952c
KhangKhangKhangKhang <duongtrongkhang121005@gmail.com>
2025-12-17 19:07:27 +0700

    fix bug level

--- src/components/ui/navBar/profileModal.jsx
+++ src/components/ui/navBar/profileModal.jsx
@@ -255,15 +255,15 @@ const ProfileModal = ({ isOpen, onClose }) => {
             <label className="block text-sm font-medium text-gray-700 mb-1">
               Trình độ
             </label>
-            <button
-              name="level"
-              value={formData.level}
-              onChange={handleChange}
-              className="w-full px-3 py-2 border bg-slate-700 text-white border-gray-300 rounded-md
-              focus:outline-none focus:ring-2 focus:ring-blue-500"
-            >
-              {formData.level}
-            </button>
+            <div>
+              <label className="block text-sm font-medium text-gray-700 mb-1">
+                Trình độ
+              </label>
+              {/* SỬA TẠI ĐÂY: Thay button bằng input */}
+              <div className="w-full px-3 py-2 border bg-slate-100 text-gray-600 border-gray-300 rounded-md select-none">
+                {formData.level}
+              </div>
+            </div>
           </div>
 
           {/* Buttons */}
